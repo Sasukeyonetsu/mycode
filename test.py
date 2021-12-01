4ef0a5310ebcbfaff4e84e3e769a439916c9d836
Sasukeyonetsu <yonetsu.sasuke@gmail.com>
2021-12-01 23:06:39 +0900

    henno

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-print("hello")
+hairetsu=[]
+hairetsu.append("sasuke")
+hairetsu.append("ffff")
+hairetsu.append("kkkkkkk")
+print(hairetsu)
+
+hairetsu.insert(1,"yonetsu")
+print(hairetsu)
+print(hairetsu[0])
+
+hairetsu.pop(0)
+print(hairetsu[0])
+print(hairetsu)
+
+for hp in hairetsu:
+    print(hp)
+
+a=5
+
+if a >= 10:
+    print("ssk")
+else:
+    print("ynt")
+
+
